a8b85097f05a986de8ba369eb7f40aa6fe537218
Sunil <sunilkumarojha444@gmail.com>
2022-03-21 18:37:16 +0530

    UC5 add

--- src/com/bridgelabz/SnakeLadderUc4.java
+++ src/com/bridgelabz/SnakeLadderUc4.java
@@ -1,5 +1,47 @@
 package com.bridgelabz;
+import java.util.Random;
 
 public class SnakeLadderUc4 {
-    
+        public static void main(String[] args) {
+            int pos = 0;
+            int dice ;
+
+            System.out.println("Single player at start position "+ pos);
+            while (pos <= 100){
+                dice = rollDice();
+                pos = mapTo(dice, pos);
+            }
+            System.out.println("Win the game pos is : "+ pos);
+        }
+        public static int rollDice(){
+            Random random = new Random();
+            int dice = random.nextInt(6);
+            dice = dice+1;
+            System.out.println("Dice Num : "+ dice);
+            return dice;
+        }
+        public static int mapTo(int dice, int pos){
+            Random random = new Random();
+            int map = random.nextInt(2);
+            map = map +1;
+            System.out.println("Maping Method : "+ map);
+
+            switch (map){
+                case 1:
+                    System.out.println("No Play You are in the same position");
+                    break;
+                case 2:
+                    pos = pos + dice;
+                    System.out.println("your position after ladder is : "+ pos);
+                    break;
+                case 3:
+                    if (pos >= 0)
+                        pos = pos - dice;
+                    System.out.println("your position after snake is : "+ pos);
+                    break;
+                default:
+
+            }
+            return pos;
+        }
 }
